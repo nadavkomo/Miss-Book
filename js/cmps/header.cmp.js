c6fe8793734b6a543f35017c8f172dd9b51fa3f1
Nadav <nadavkomo@gmail.com>
2021-01-18 15:42:37 +0200

    nav

--- js/cmps/header.cmp.js
+++ js/cmps/header.cmp.js
@@ -3,7 +3,6 @@ export default {
             <section class="main-header">
                 <button class="logo" @click="toHome()">Miss Book</button>
                 <nav>
-                  <router-link to='/' exact>Home</router-link>
                   <router-link to='/book' exact>Book App</router-link>
                   <router-link to='/about' exact>About Us</router-link>
                 </nav>
